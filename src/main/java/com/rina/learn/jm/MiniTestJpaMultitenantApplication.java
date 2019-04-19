3c1bffc4a6d77517f9bfd62f5c07453f58178e54
Rina Zhao <rina.zhao@sap.com>
2019-04-19 14:28:14 +0800

    first install success

---
+++ src/main/java/com/rina/learn/jm/MiniTestJpaMultitenantApplication.java
@@ -0,0 +1,13 @@
+package com.rina.learn.jm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniTestJpaMultitenantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniTestJpaMultitenantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rina/learn/jm/MiniTestJpaMultitenantApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rina.learn.jm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@WebMvcTest
+public class MiniTestJpaMultitenantApplicationTests {
+
+//	@Test
+//	public void contextLoads() {
+//	}
+
+}
